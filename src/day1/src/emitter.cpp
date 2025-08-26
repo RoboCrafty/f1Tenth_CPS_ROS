ae72555cbf11e27f7d5c22307423135339b9c339
RoboCrafty <fuzailhamid2002@gmail.com>
2025-08-26 12:27:22 +0200

    Day 1 --> "Altering the behavior"

--- src/day1/src/emitter.cpp
+++ src/day1/src/emitter.cpp
@@ -1,22 +1,38 @@
 #include "ros/ros.h"
 #include "std_msgs/Int32.h"
+#include "iostream"
+#include "random"
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "Emitter");
     ros::NodeHandle node;
 
     ros::Publisher pub = node.advertise<std_msgs::Int32>("odd", 10);
-    ros::Rate rate(100); // 1 Hz
+    ros::Rate rate(3); // 1 Hz
 
-    int num = 42;
+    // std::random_device rand_gen;
+    // std::mt19937 mt(rand_gen());
+    // std::uniform_int_distribution distribution(1, 100000);
+
+    
+    int num;
    
 
     while (ros::ok()) {
+        num = rand ();
 
         std_msgs::Int32 msg;
         msg.data = num;
         
-        pub.publish(msg);
+        if (num%2==0)
+        {
+        }
+
+        else
+        {
+            pub.publish(msg);
+        }
+        
         
         rate.sleep();
         
